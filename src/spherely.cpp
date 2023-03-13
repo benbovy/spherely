9a31028cc0127498fcb691ce1fb3527a4ba49512
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2023-03-13 11:24:55 +0100

    Add support for getting projected coordinates

--- src/spherely.cpp
+++ src/spherely.cpp
@@ -7,6 +7,8 @@ namespace py = pybind11;
 
 void init_geography(py::module&);
 void init_predicates(py::module&);
+void init_projections(py::module&);
+
 
 PYBIND11_MODULE(spherely, m) {
     m.doc() = R"pbdoc(
@@ -19,6 +21,7 @@ PYBIND11_MODULE(spherely, m) {
 
     init_geography(m);
     init_predicates(m);
+    init_projections(m);
 
 #ifdef VERSION_INFO
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
