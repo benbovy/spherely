11b0fbb447f4688ce54e3c58fa2a6e3f15e410ff
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2023-03-18 21:17:32 +0100

    add centroid and boundary

--- src/accessors-geog.cpp
+++ src/accessors-geog.cpp
@@ -7,6 +7,22 @@ namespace py = pybind11;
 namespace s2geog = s2geography;
 using namespace spherely;
 
+PyObjectGeography centroid(PyObjectGeography a) {
+    const auto& a_ptr = a.as_geog_ptr()->geog();
+    auto s2_point = s2geog::s2_centroid(a_ptr);
+    std::unique_ptr<Point> point =
+        make_geography<s2geog::PointGeography, spherely::Point>(s2_point);
+    return PyObjectGeography::from_geog(std::move(point));
+}
+
+PyObjectGeography boundary(PyObjectGeography a) {
+    const auto& a_ptr = a.as_geog_ptr()->geog();
+    auto s2_obj = s2geog::s2_boundary(a_ptr);
+    // TODO return specific subclass
+    auto geog_ptr = std::make_unique<spherely::Geography>(std::move(s2_obj));
+    return PyObjectGeography::from_geog(std::move(geog_ptr));
+}
+
 PyObjectGeography convex_hull(PyObjectGeography a) {
     const auto& a_ptr = a.as_geog_ptr()->geog();
     auto s2_obj = s2geog::s2_convex_hull(a_ptr);
@@ -15,6 +31,32 @@ PyObjectGeography convex_hull(PyObjectGeography a) {
 }
 
 void init_accessors(py::module& m) {
+    m.def("centroid",
+          py::vectorize(&centroid),
+          py::arg("a"),
+          R"pbdoc(
+        Computes the centroid of each geography.
+
+        Parameters
+        ----------
+        a : :py:class:`Geography` or array_like
+            Geography object
+
+    )pbdoc");
+
+    m.def("boundary",
+          py::vectorize(&boundary),
+          py::arg("a"),
+          R"pbdoc(
+        Computes the boundary of each geography.
+
+        Parameters
+        ----------
+        a : :py:class:`Geography` or array_like
+            Geography object
+
+    )pbdoc");
+
     m.def("convex_hull",
           py::vectorize(&convex_hull),
           py::arg("a"),
