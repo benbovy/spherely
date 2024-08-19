7a9607b3049942df2b724626820fb35e6c7cc943
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2024-08-19 14:44:20 +0200

    try fix typing

--- tests/test_coords.py
+++ tests/test_coords.py
@@ -11,16 +11,17 @@ def test_get_lat_lng() -> None:
     assert spherely.get_lng(a) == pytest.approx(2.6, abs=1e-14)
 
     # array
-    a = np.array([spherely.Point(0, 1), spherely.Point(1, 2), spherely.Point(2, 3)])
+    arr = np.array([spherely.Point(0, 1), spherely.Point(1, 2), spherely.Point(2, 3)])
 
-    actual = spherely.get_lat(a)
+    actual = spherely.get_lat(arr)
     expected = np.array([0, 1, 2], dtype="float64")
     np.testing.assert_allclose(actual, expected)
 
-    actual = spherely.get_lng(a)
+    actual = spherely.get_lng(arr)
     expected = np.array([1, 2, 3], dtype="float64")
     np.testing.assert_allclose(actual, expected)
 
+    # only points are supported
     with pytest.raises(ValueError):
         spherely.get_lat(spherely.LineString([(0, 1), (1, 2)]))
 
