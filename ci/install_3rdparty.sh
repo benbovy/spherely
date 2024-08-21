fb658d0b6a1375f4bcb249c9564a448adf5c13c7
Benoit Bovy <benbovy@gmail.com>
2024-08-21 15:40:10 +0200

    CI: build sdist and wheels (Linux & MacOS) (#35)
    * ci: build wheels (wip)
    
    * disable running cmake for sdist
    
    * sdist: add missing --sdist option
    
    * fix 3rd-party directory
    
    * fix s2geography version
    
    * fix s2geography tag
    
    * fix ci install bash script permission
    
    * fix install script fn name
    
    * typo
    
    * download dependencies in build script
    
    * build script: cd into root dependencies dir
    
    * fix tar?
    
    * fix deps download hopefully
    
    * create source directory before download
    
    * typo
    
    * typos and old cmake behavior for cmake _ROOT
    
    * openssl (header files) seems missing
    
    * revert previous commit, set OPENSSL_ROOT_DIR on linux
    
    * download & build openssl in dependencies script
    
    * revert building openssl, try ubuntu-latest
    
    * try force reinstall openssl
    
    * install openssl in linux container using yum
    
    * typo
    
    * make installed deps discoverable more globally
    
    Both inside the install_3rdparty script and for building the python
    extension.
    
    * try fix openssl transitive headers not found
    
    ci build wheels errors on macOS.
    
    * another attempt at fixing openssl header lookup
    
    * repair wheel: add 3rd-party install path
    
    * update s2geography patch
    
    * also require openssl in spherely
    
    Fix openssl headers not found when compiling spherely on macOS (ci
    Python wheels).
    
    * audithwheel command not found?? -> typo!
    
    * auditwheel not on macOS
    
    * typo
    
    * more logs
    
    * fix wheel repair on linux and macos?
    
    * repair wheels: use DYLD_LIBRARY_PATH on macos
    
    * bump minimum required python version to 3.10
    
    * fix pytest tests location?
    
    * bump macos deployment target
    
    Unfortunately we cannot set lower target versions until we use an
    alternative way to install openssl (e.g., build it from source). OpenSSL
    Homebrew packages were built with higher target versions.
    
    * fix pytest command?
    
    Co-authored-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>
    
    * update patch (upstream ci fix suggestion)
    
    * add upload step
    
    * revert last change in patch
    
    * add 3rd-party licenses in the built wheels
    
    * add ci job upload release files to pypi
    
    ---------
    
    Co-authored-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>

---
+++ ci/install_3rdparty.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+# Build and install absl, s2 and s2geography on posix systems.
+#
+# This script requires environment variables to be set
+#  - DEPENDENCIES_DIR=/path/to/cached/prefix -- to build or use as cache
+#  - ABSL_VERSION
+#  - S2GEOMETRY_VERSION
+#  - S2GEOGRAPHY_VERSION
+#  - CXX_STANDARD
+#
+# This script assumes that library sources have been downloaded or copied in
+# DEPENDENCIES_DIR (e.g., $DEPENDENCIES_DIR/absl-src-$ABSL_VERSION).
+pushd .
+
+set -e
+
+if [ -z "$DEPENDENCIES_DIR" ]; then
+    echo "DEPENDENCIES_DIR must be set"
+    exit 1
+elif [ -z "$ABSL_VERSION" ]; then
+    echo "ABSL_VERSION must be set"
+    exit 1
+elif [ -z "$S2GEOMETRY_VERSION" ]; then
+    echo "S2GEOMETRY_VERSION must be set"
+    exit 1
+elif [ -z "$S2GEOGRAPHY_VERSION" ]; then
+    echo "S2GEOGRAPHY_VERSION must be set"
+    exit 1
+elif [ -z "$CXX_STANDARD" ]; then
+    echo "CXX_STANDARD must be set"
+    exit 1
+fi
+
+SRC_DIR=$DEPENDENCIES_DIR/src
+BUILD_DIR=$DEPENDENCIES_DIR/build
+INSTALL_DIR=$DEPENDENCIES_DIR/dist
+
+mkdir -p $SRC_DIR
+mkdir -p $BUILD_DIR
+mkdir -p $INSTALL_DIR
+
+ABSL_SRC_DIR=$DEPENDENCIES_DIR/absl-src-$ABSL_VERSION
+S2GEOMETRY_SRC_DIR=$DEPENDENCIES_DIR/s2geometry-src-$S2GEOMETRY_VERSION
+S2GEOGRAPHY_SRC_DIR=$DEPENDENCIES_DIR/s2geography-src-$S2GEOGRAPHY_VERSION
+
+ABSL_BUILD_DIR=$BUILD_DIR/absl-src-$ABSL_VERSION
+S2GEOMETRY_BUILD_DIR=$BUILD_DIR/s2geometry-src-$S2GEOMETRY_VERSION
+S2GEOGRAPHY_BUILD_DIR=$BUILD_DIR/s2geography-src-$S2GEOGRAPHY_VERSION
+
+build_install_dependencies(){
+    echo "----- Installing cmake"
+    pip install cmake
+
+    echo "----- Installing OpenSSL in Linux container"
+
+    if [ "$(uname)" != "Darwin" ]; then
+        # assume manylinux2014 https://cibuildwheel.pypa.io/en/stable/faq/
+        yum install -y openssl-devel
+    fi
+
+    echo "------ Clean build and install directories"
+
+    rm -rf $BUILD_DIR/*
+    rm -rf $INSTALL_DIR/*
+
+    echo "----- Downloading, building and installing absl-$ABSL_VERSION"
+
+    cd $DEPENDENCIES_DIR
+    curl -o absl.tar.gz -L https://github.com/abseil/abseil-cpp/archive/refs/tags/$ABSL_VERSION.tar.gz
+    tar -xf absl.tar.gz -C $SRC_DIR
+    rm -f absl.tar.gz
+
+    cmake -S $SRC_DIR/abseil-cpp-$ABSL_VERSION -B $ABSL_BUILD_DIR \
+        -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+        -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+        -DCMAKE_CXX_STANDARD=$CXX_STANDARD \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DABSL_ENABLE_INSTALL=ON
+
+    cmake --build $ABSL_BUILD_DIR
+    cmake --install $ABSL_BUILD_DIR
+
+    echo "----- Downloading, building and installing s2geometry-$S2GEOMETRY_VERSION"
+
+    cd $DEPENDENCIES_DIR
+    curl -o s2geometry.tar.gz -L https://github.com/google/s2geometry/archive/refs/tags/v$S2GEOMETRY_VERSION.tar.gz
+    tar -xf s2geometry.tar.gz -C $SRC_DIR
+    rm -f s2geometry.tar.gz
+
+    cmake -S $SRC_DIR/s2geometry-$S2GEOMETRY_VERSION -B $S2GEOMETRY_BUILD_DIR \
+        -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+        -DBUILD_TESTS=OFF \
+        -DBUILD_EXAMPLES=OFF \
+        -UGOOGLETEST_ROOT \
+        -DCMAKE_CXX_STANDARD=$CXX_STANDARD \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DBUILD_SHARED_LIBS=ON
+
+    cmake --build $S2GEOMETRY_BUILD_DIR
+    cmake --install $S2GEOMETRY_BUILD_DIR
+
+    echo "----- Downloading, building and installing s2geography-$S2GEOGRAPHY_VERSION"
+
+    cd $DEPENDENCIES_DIR
+    curl -o s2geography.tar.gz -L https://github.com/paleolimbot/s2geography/archive/refs/tags/$S2GEOGRAPHY_VERSION.tar.gz
+    tar -xf s2geography.tar.gz -C $SRC_DIR
+    rm -f s2geography.tar.gz
+
+    # TODO: remove when fixed in s2geography
+    cd $SRC_DIR/s2geography-$S2GEOGRAPHY_VERSION
+    if [ "$(uname)" == "Darwin" ]; then
+        patch -p1 < $PROJECT_DIR/ci/s2geography-add-openssl-as-requirement.patch
+    else
+        patch -p1 < /project/ci/s2geography-add-openssl-as-requirement.patch
+    fi
+
+    cmake -S $SRC_DIR/s2geography-$S2GEOGRAPHY_VERSION -B $S2GEOGRAPHY_BUILD_DIR \
+        -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+        -DS2GEOGRAPHY_BUILD_TESTS=OFF \
+        -DS2GEOGRAPHY_S2_SOURCE=AUTO \
+        -DS2GEOGRAPHY_BUILD_EXAMPLES=OFF \
+        -DCMAKE_CXX_STANDARD=$CXX_STANDARD \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DBUILD_SHARED_LIBS=ON
+
+    cmake --build $S2GEOGRAPHY_BUILD_DIR
+    cmake --install $S2GEOGRAPHY_BUILD_DIR
+}
+
+if [ -d "$INSTALL_DIR/include/s2geography" ]; then
+    echo "Using cached install directory $INSTALL_DIR"
+else
+    build_install_dependencies
+fi
+
+popd
