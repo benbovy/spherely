2b9922deda65b823addb73386f9ad3819afcb1b6
Benoit Bovy <benbovy@gmail.com>
2024-09-04 11:27:07 +0200

    install s2geometry openssl abseil with vcpkg

--- ci/install_3rdparty.cmd
+++ ci/install_3rdparty.cmd
@@ -23,62 +23,62 @@ mkdir %SRC_DIR%
 mkdir %BUILD_DIR%
 mkdir %INSTALL_DIR%
 
-set ABSL_SRC_DIR=%DEPENDENCIES_DIR%\absl-src-%ABSL_VERSION%
-set S2GEOMETRY_SRC_DIR=%DEPENDENCIES_DIR%\s2geometry-src-%S2GEOMETRY_VERSION%
+rem set ABSL_SRC_DIR=%DEPENDENCIES_DIR%\absl-src-%ABSL_VERSION%
+rem set S2GEOMETRY_SRC_DIR=%DEPENDENCIES_DIR%\s2geometry-src-%S2GEOMETRY_VERSION%
 set S2GEOGRAPHY_SRC_DIR=%DEPENDENCIES_DIR%\s2geography-src-%S2GEOGRAPHY_VERSION%
 
-set ABSL_BUILD_DIR=%BUILD_DIR%\absl-src-%ABSL_VERSION%
-set S2GEOMETRY_BUILD_DIR=%BUILD_DIR%\s2geometry-src-%S2GEOMETRY_VERSION%
+rem set ABSL_BUILD_DIR=%BUILD_DIR%\absl-src-%ABSL_VERSION%
+rem set S2GEOMETRY_BUILD_DIR=%BUILD_DIR%\s2geometry-src-%S2GEOMETRY_VERSION%
 set S2GEOGRAPHY_BUILD_DIR=%BUILD_DIR%\s2geography-src-%S2GEOGRAPHY_VERSION%
 
 echo "----- Installing cmake"
 pip install ninja cmake
 
-echo "----- Downloading, building and installing absl-%ABSL_VERSION%"
+rem echo "----- Downloading, building and installing absl-%ABSL_VERSION%"
 
-cd %DEPENDENCIES_DIR%
-curl -o absl.tar.gz -L https://github.com/abseil/abseil-cpp/archive/refs/tags/%ABSL_VERSION%.tar.gz
-tar -xf absl.tar.gz -C %SRC_DIR%
+rem cd %DEPENDENCIES_DIR%
+rem curl -o absl.tar.gz -L https://github.com/abseil/abseil-cpp/archive/refs/tags/%ABSL_VERSION%.tar.gz
+rem tar -xf absl.tar.gz -C %SRC_DIR%
 
-cmake -GNinja ^
-    -S %SRC_DIR%/abseil-cpp-%ABSL_VERSION% ^
-    -B %ABSL_BUILD_DIR% ^
-    -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% ^
-    -DCMAKE_POSITION_INDEPENDENT_CODE=ON ^
-    -DCMAKE_CXX_STANDARD=%CXX_STANDARD% ^
-    -DCMAKE_BUILD_TYPE=Release ^
-    -DABSL_ENABLE_INSTALL=ON
+rem cmake -GNinja ^
+rem     -S %SRC_DIR%/abseil-cpp-%ABSL_VERSION% ^
+rem     -B %ABSL_BUILD_DIR% ^
+rem     -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% ^
+rem     -DCMAKE_POSITION_INDEPENDENT_CODE=ON ^
+rem     -DCMAKE_CXX_STANDARD=%CXX_STANDARD% ^
+rem     -DCMAKE_BUILD_TYPE=Release ^
+rem     -DABSL_ENABLE_INSTALL=ON
 
-IF %ERRORLEVEL% NEQ 0 exit /B 1
-cmake --build %ABSL_BUILD_DIR%
-IF %ERRORLEVEL% NEQ 0 exit /B 2
-cmake --install %ABSL_BUILD_DIR%
+rem IF %ERRORLEVEL% NEQ 0 exit /B 1
+rem cmake --build %ABSL_BUILD_DIR%
+rem IF %ERRORLEVEL% NEQ 0 exit /B 2
+rem cmake --install %ABSL_BUILD_DIR%
 
-echo "----- Downloading, building and installing s2geometry-%S2GEOMETRY_VERSION%"
+rem echo "----- Downloading, building and installing s2geometry-%S2GEOMETRY_VERSION%"
 
-echo %OPENSSL_ROOT_DIR%
+rem echo %OPENSSL_ROOT_DIR%
 
-cd %DEPENDENCIES_DIR%
-curl -o s2geometry.tar.gz -L https://github.com/google/s2geometry/archive/refs/tags/v%S2GEOMETRY_VERSION%.tar.gz
-tar -xf s2geometry.tar.gz -C %SRC_DIR%
+rem cd %DEPENDENCIES_DIR%
+rem curl -o s2geometry.tar.gz -L https://github.com/google/s2geometry/archive/refs/tags/v%S2GEOMETRY_VERSION%.tar.gz
+rem tar -xf s2geometry.tar.gz -C %SRC_DIR%
 
-cmake -GNinja ^
-    -S %SRC_DIR%/s2geometry-%S2GEOMETRY_VERSION% ^
-    -B %S2GEOMETRY_BUILD_DIR% ^
-    -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% ^
-    -DOPENSSL_ROOT_DIR=%OPENSSL_ROOT_DIR% ^
-    -DBUILD_TESTS=OFF ^
-    -DBUILD_EXAMPLES=OFF ^
-    -UGOOGLETEST_ROOT ^
-    -DCMAKE_CXX_STANDARD=%CXX_STANDARD% ^
-    -DCMAKE_BUILD_TYPE=Release ^
-    -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE ^
-    -DBUILD_SHARED_LIBS=ON
+rem cmake -GNinja ^
+rem     -S %SRC_DIR%/s2geometry-%S2GEOMETRY_VERSION% ^
+rem     -B %S2GEOMETRY_BUILD_DIR% ^
+rem     -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% ^
+rem     -DOPENSSL_ROOT_DIR=%OPENSSL_ROOT_DIR% ^
+rem     -DBUILD_TESTS=OFF ^
+rem     -DBUILD_EXAMPLES=OFF ^
+rem     -UGOOGLETEST_ROOT ^
+rem     -DCMAKE_CXX_STANDARD=%CXX_STANDARD% ^
+rem     -DCMAKE_BUILD_TYPE=Release ^
+rem     -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE ^
+rem     -DBUILD_SHARED_LIBS=ON
 
-IF %ERRORLEVEL% NEQ 0 exit /B 3
-cmake --build %S2GEOMETRY_BUILD_DIR%
-IF %ERRORLEVEL% NEQ 0 exit /B 4
-cmake --install %S2GEOMETRY_BUILD_DIR%
+rem IF %ERRORLEVEL% NEQ 0 exit /B 3
+rem cmake --build %S2GEOMETRY_BUILD_DIR%
+rem IF %ERRORLEVEL% NEQ 0 exit /B 4
+rem cmake --install %S2GEOMETRY_BUILD_DIR%
 
 echo "----- Downloading, building and installing s2geography-%S2GEOGRAPHY_VERSION%"
 
