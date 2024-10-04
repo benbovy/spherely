d69b272ba7500a9670a55d5de78c92fa94166c45
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2024-10-04 15:50:27 +0200

    init_geoarrow only when available

--- src/spherely.cpp
+++ src/spherely.cpp
@@ -8,7 +8,10 @@ namespace py = pybind11;
 void init_geography(py::module&);
 void init_predicates(py::module&);
 void init_accessors(py::module&);
+#if defined(S2GEOGRAPHY_VERSION_MAJOR) && \
+    (S2GEOGRAPHY_VERSION_MAJOR >= 1 || S2GEOGRAPHY_VERSION_MINOR >= 2)
 void init_geoarrow(py::module&);
+#endif
 
 PYBIND11_MODULE(spherely, m) {
     m.doc() = R"pbdoc(
@@ -22,7 +25,10 @@ PYBIND11_MODULE(spherely, m) {
     init_geography(m);
     init_predicates(m);
     init_accessors(m);
+#if defined(S2GEOGRAPHY_VERSION_MAJOR) && \
+    (S2GEOGRAPHY_VERSION_MAJOR >= 1 || S2GEOGRAPHY_VERSION_MINOR >= 2)
     init_geoarrow(m);
+#endif
 
 #ifdef VERSION_INFO
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
