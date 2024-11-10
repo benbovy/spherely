db351e65b2d78fcd0ca7ff0ed211ce67ef97c844
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2024-11-10 15:58:54 +0100

    ENH: add to_wkb / from_wkb

--- src/io.cpp
+++ src/io.cpp
@@ -54,6 +54,51 @@ private:
     std::shared_ptr<s2geog::WKTWriter> m_writer;
 };
 
+#if defined(S2GEOGRAPHY_VERSION_MAJOR) && \
+    (S2GEOGRAPHY_VERSION_MAJOR >= 1 || S2GEOGRAPHY_VERSION_MINOR >= 2)
+class FromWKB {
+public:
+    FromWKB(bool oriented, bool planar, float tessellate_tolerance = 100.0) {
+        s2geog::geoarrow::ImportOptions options;
+        options.set_oriented(oriented);
+        if (planar) {
+            auto tol = S1Angle::Radians(tessellate_tolerance / EARTH_RADIUS_METERS);
+            options.set_tessellate_tolerance(tol);
+        }
+        m_reader = std::make_shared<s2geog::WKBReader>(options);
+    }
+
+    PyObjectGeography operator()(py::bytes a) const {
+        return make_py_geography(m_reader->ReadFeature(a));
+    }
+
+private:
+    std::shared_ptr<s2geog::WKBReader> m_reader;
+};
+
+class ToWKB {
+public:
+    ToWKB() {
+        m_writer = std::make_shared<s2geog::WKBWriter>();
+    }
+
+    py::bytes operator()(PyObjectGeography a) const {
+        auto res = m_writer->WriteFeature(a.as_geog_ptr()->geog());
+        // manually convert basic_string to python bytes object
+        auto res_py = PyBytes_FromStringAndSize(reinterpret_cast<const char*>(res.data()),
+                                                static_cast<ssize_t>(res.size()));
+        if (!res_py) {
+            throw py::error_already_set();
+        }
+        return py::reinterpret_steal<py::bytes>(res_py);
+    }
+
+private:
+    std::shared_ptr<s2geog::WKBWriter> m_writer;
+};
+
+#endif
+
 void init_io(py::module& m) {
     m.def(
         "from_wkt",
@@ -109,4 +154,58 @@ void init_io(py::module& m) {
             The number of decimal places to include in the output.
 
     )pbdoc");
+
+#if defined(S2GEOGRAPHY_VERSION_MAJOR) && \
+    (S2GEOGRAPHY_VERSION_MAJOR >= 1 || S2GEOGRAPHY_VERSION_MINOR >= 2)
+
+    m.def(
+        "from_wkb",
+        [](py::array_t<py::bytes> a, bool oriented, bool planar, float tessellate_tolerance) {
+            return py::vectorize(FromWKB(oriented, planar, tessellate_tolerance))(std::move(a));
+        },
+        py::arg("a"),
+        py::arg("oriented") = false,
+        py::arg("planar") = false,
+        py::arg("tessellate_tolerance") = 100.0,
+        R"pbdoc(
+        Creates geographies from the Well-Known Bytes (WKB) representation.
+
+        Parameters
+        ----------
+        a : bytes or array_like
+            WKB objects.
+        oriented : bool, default False
+            Set to True if polygon ring directions are known to be correct
+            (i.e., exterior rings are defined counter clockwise and interior
+            rings are defined clockwise).
+            By default (False), it will return the polygon with the smaller
+            area.
+        planar : bool, default False
+            If set to True, the edges of linestrings and polygons are assumed
+            to be linear on the plane. In that case, additional points will
+            be added to the line while creating the geography objects, to
+            ensure every point is within 100m of the original line.
+            By default (False), it is assumed that the edges are spherical
+            (i.e. represent the shortest path on the sphere between two points).
+        tessellate_tolerance : float, default 100.0
+            The maximum distance in meters that a point must be moved to
+            satisfy the planar edge constraint. This is only used if `planar`
+            is set to True.
+
+    )pbdoc");
+
+    m.def("to_wkb",
+          py::vectorize(ToWKB()),
+          py::arg("a"),
+          R"pbdoc(
+        Returns the WKB representation of each geography.
+
+        Parameters
+        ----------
+        a : :py:class:`Geography` or array_like
+            Geography object(s)
+
+    )pbdoc");
+
+#endif
 }
--- tests/test_io.py
+++ tests/test_io.py
@@ -1,3 +1,5 @@
+import struct
+
 import numpy as np
 import pytest
 from packaging.version import Version
@@ -108,3 +110,79 @@ def test_to_wkt_precision():
 
     result = spherely.to_wkt(arr, precision=2)
     assert result[0] == "POINT (0.12 0.57)"
+
+
+POINT11_WKB = struct.pack("<BI2d", 1, 1, 1.0, 1.0)
+NAN = struct.pack("<d", float("nan"))
+POINT_NAN_WKB = struct.pack("<BI", 1, 1) + (NAN * 2)
+MULTIPOINT_NAN_WKB = struct.pack("<BII", 1, 4, 1) + POINT_NAN_WKB
+GEOMETRYCOLLECTION_NAN_WKB = struct.pack("<BII", 1, 7, 1) + POINT_NAN_WKB
+INVALID_WKB = bytes.fromhex(
+    "01030000000100000002000000507daec600b1354100de02498e5e3d41306ea321fcb03541a011a53d905e3d41"
+)  # noqa: E501
+
+
+def test_from_wkb_point_empty():
+    result = spherely.from_wkb([POINT11_WKB, POINT_NAN_WKB, MULTIPOINT_NAN_WKB])
+    # empty MultiPoint is converted to empty Point
+    expected = spherely.from_wkt(["POINT (1 1)", "POINT EMPTY", "POINT EMPTY"])
+    assert spherely.equals(result, expected).all()
+
+    result = spherely.from_wkb(GEOMETRYCOLLECTION_NAN_WKB)
+    assert str(result) == "GEOMETRYCOLLECTION (POINT EMPTY)"
+
+
+def test_from_wkb_invalid():
+    with pytest.raises(RuntimeError, match="Expected endian byte"):
+        spherely.from_wkb(b"")
+
+    with pytest.raises(RuntimeError):
+        spherely.from_wkb([b"\x01\x01\x00\x00\x00\x00"])
+
+    # TODO should this raise an error?
+    # with pytest.raises(RuntimeError):
+    result = spherely.from_wkb(INVALID_WKB)
+    assert str(result) == "POLYGON ((108.7761 -10.2852, 108.7761 -10.2852))"
+
+
+def test_from_wkb_invalid_type():
+    with pytest.raises(TypeError, match="expected bytes, str found"):
+        spherely.from_wkb("POINT (1 1)")
+
+
+@pytest.mark.parametrize(
+    "geog",
+    [
+        spherely.point(45, 50),
+        spherely.multipoint([(5, 50), (6, 51)]),
+        spherely.linestring([(5, 50), (6, 51)]),
+        spherely.multilinestring([[(5, 50), (6, 51)], [(15, 60), (16, 61)]]),
+        spherely.polygon([(5, 50), (5, 60), (6, 60), (6, 51)]),
+        # with hole
+        spherely.polygon(
+            shell=[(5, 60), (6, 60), (6, 50), (5, 50)],
+            holes=[[(5.1, 59), (5.9, 59), (5.9, 51), (5.1, 51)]],
+        ),
+        spherely.multipolygon(
+            [
+                spherely.polygon([(5, 50), (5, 60), (6, 60), (6, 51)]),
+                spherely.polygon([(10, 100), (10, 160), (11, 160), (11, 100)]),
+            ]
+        ),
+        spherely.collection([spherely.point(40, 50)]),
+        spherely.collection(
+            [
+                spherely.point(0, 0),
+                spherely.linestring([(0, 0), (1, 1)]),
+                spherely.polygon([(0, 0), (1, 0), (1, 1)]),
+            ]
+        ),
+    ],
+)
+def test_wkb_roundtrip(geog):
+    wkb = spherely.to_wkb(geog)
+    result = spherely.from_wkb(wkb)
+    # roundtrip through Geography unit vector is not exact, so equals can fail
+    # TODO properly test this once `equals` supports snapping/precision
+    # assert spherely.equals(result, geog)
+    assert str(result) == str(geog)
