53bf81a643c653c09e1cfa568c0d0d49687d9dcb
Benoit Bovy <benbovy@gmail.com>
2024-12-05 21:40:19 +0100

    Rename scalar creation functions (#83)

--- tests/test_accessors.py
+++ tests/test_accessors.py
@@ -9,14 +9,14 @@ import spherely
 @pytest.mark.parametrize(
     "geog, expected",
     [
-        (spherely.point(0, 0), spherely.point(0, 0)),
+        (spherely.create_point(0, 0), spherely.create_point(0, 0)),
         (
-            spherely.linestring([(0, 0), (2, 0)]),
-            spherely.point(1, 0),
+            spherely.create_linestring([(0, 0), (2, 0)]),
+            spherely.create_point(1, 0),
         ),
         (
-            spherely.polygon([(0, 0), (0, 2), (2, 2), (2, 0)]),
-            spherely.point(1, 1),
+            spherely.create_polygon([(0, 0), (0, 2), (2, 2), (2, 0)]),
+            spherely.create_point(1, 1),
         ),
     ],
 )
@@ -38,10 +38,13 @@ def test_centroid(geog, expected) -> None:
 @pytest.mark.parametrize(
     "geog, expected",
     [
-        (spherely.point(0, 0), "GEOMETRYCOLLECTION EMPTY"),
-        (spherely.linestring([(0, 0), (2, 0), (2, 2)]), "MULTIPOINT ((0 0), (2 2))"),
+        (spherely.create_point(0, 0), "GEOMETRYCOLLECTION EMPTY"),
         (
-            spherely.polygon([(0, 0), (0, 2), (2, 2), (0.5, 1.5)]),
+            spherely.create_linestring([(0, 0), (2, 0), (2, 2)]),
+            "MULTIPOINT ((0 0), (2 2))",
+        ),
+        (
+            spherely.create_polygon([(0, 0), (0, 2), (2, 2), (0.5, 1.5)]),
             "LINESTRING (0.5 1.5, 2 2, 0 2, 0 0, 0.5 1.5)",
         ),
     ],
@@ -61,12 +64,12 @@ def test_boundary(geog, expected) -> None:
     "geog, expected",
     [
         (
-            spherely.linestring([(0, 0), (2, 0), (2, 2)]),
-            spherely.polygon([(0, 0), (2, 0), (2, 2)]),
+            spherely.create_linestring([(0, 0), (2, 0), (2, 2)]),
+            spherely.create_polygon([(0, 0), (2, 0), (2, 2)]),
         ),
         (
-            spherely.polygon([(0, 0), (0, 2), (2, 2), (0.5, 1.5)]),
-            spherely.polygon([(0, 0), (0, 2), (2, 2)]),
+            spherely.create_polygon([(0, 0), (0, 2), (2, 2), (0.5, 1.5)]),
+            spherely.create_polygon([(0, 0), (0, 2), (2, 2)]),
         ),
     ],
 )
@@ -86,12 +89,18 @@ def test_convex_hull(geog, expected) -> None:
 
 def test_get_x_y() -> None:
     # scalar
-    a = spherely.point(1.5, 2.6)
+    a = spherely.create_point(1.5, 2.6)
     assert spherely.get_x(a) == pytest.approx(1.5, abs=1e-14)
     assert spherely.get_y(a) == pytest.approx(2.6, abs=1e-14)
 
     # array
-    arr = np.array([spherely.point(0, 1), spherely.point(1, 2), spherely.point(2, 3)])
+    arr = np.array(
+        [
+            spherely.create_point(0, 1),
+            spherely.create_point(1, 2),
+            spherely.create_point(2, 3),
+        ]
+    )
 
     actual = spherely.get_x(arr)
     expected = np.array([0, 1, 2], dtype="float64")
@@ -103,28 +112,28 @@ def test_get_x_y() -> None:
 
     # only points are supported
     with pytest.raises(ValueError):
-        spherely.get_x(spherely.linestring([(0, 1), (1, 2)]))
+        spherely.get_x(spherely.create_linestring([(0, 1), (1, 2)]))
 
     with pytest.raises(ValueError):
-        spherely.get_y(spherely.linestring([(0, 1), (1, 2)]))
+        spherely.get_y(spherely.create_linestring([(0, 1), (1, 2)]))
 
 
 @pytest.mark.parametrize(
     "geog_a, geog_b, expected",
     [
         (
-            spherely.point(0, 0),
-            spherely.point(0, 90),
+            spherely.create_point(0, 0),
+            spherely.create_point(0, 90),
             np.pi / 2 * spherely.EARTH_RADIUS_METERS,
         ),
         (
-            spherely.point(0, 90),
-            spherely.point(90, 30),
+            spherely.create_point(0, 90),
+            spherely.create_point(90, 30),
             np.pi / 3 * spherely.EARTH_RADIUS_METERS,
         ),
         (
-            spherely.polygon([(0, 0), (30, 60), (60, -30)]),
-            spherely.point(0, 90),
+            spherely.create_polygon([(0, 0), (30, 60), (60, -30)]),
+            spherely.create_point(0, 90),
             np.pi / 6 * spherely.EARTH_RADIUS_METERS,
         ),
     ],
@@ -145,8 +154,8 @@ def test_distance(geog_a, geog_b, expected) -> None:
 
 def test_distance_with_custom_radius() -> None:
     actual = spherely.distance(
-        spherely.point(0, 90),
-        spherely.point(0, 0),
+        spherely.create_point(0, 90),
+        spherely.create_point(0, 0),
         radius=1,
     )
     assert isinstance(actual, float)
@@ -155,7 +164,7 @@ def test_distance_with_custom_radius() -> None:
 
 def test_area():
     # scalar
-    geog = spherely.polygon([(0, 0), (90, 0), (0, 90), (0, 0)])
+    geog = spherely.create_polygon([(0, 0), (90, 0), (0, 90), (0, 0)])
     result = spherely.area(geog, radius=1)
     assert isinstance(result, float)
     expected = 4 * math.pi / 8
@@ -187,7 +196,7 @@ def test_area_empty(geog):
 
 
 def test_length():
-    geog = spherely.linestring([(0, 0), (1, 0)])
+    geog = spherely.create_linestring([(0, 0), (1, 0)])
     result = spherely.length(geog, radius=1)
     assert isinstance(result, float)
     expected = 1.0 * np.pi / 180.0
@@ -214,7 +223,7 @@ def test_length_invalid(geog):
 
 
 def test_perimeter():
-    geog = spherely.polygon([(0, 0), (0, 90), (90, 90), (90, 0), (0, 0)])
+    geog = spherely.create_polygon([(0, 0), (0, 90), (90, 90), (90, 0), (0, 0)])
     result = spherely.perimeter(geog, radius=1)
     assert isinstance(result, float)
     expected = 3 * 90 * np.pi / 180.0
--- tests/test_predicates.py
+++ tests/test_predicates.py
@@ -8,19 +8,19 @@ def test_intersects() -> None:
     # test array + scalar
     a = np.array(
         [
-            spherely.linestring([(40, 8), (60, 8)]),
-            spherely.linestring([(20, 0), (30, 0)]),
+            spherely.create_linestring([(40, 8), (60, 8)]),
+            spherely.create_linestring([(20, 0), (30, 0)]),
         ]
     )
-    b = spherely.linestring([(50, 5), (50, 10)])
+    b = spherely.create_linestring([(50, 5), (50, 10)])
 
     actual = spherely.intersects(a, b)
     expected = np.array([True, False])
     np.testing.assert_array_equal(actual, expected)
 
     # two scalars
-    a2 = spherely.point(50, 8)
-    b2 = spherely.point(20, 5)
+    a2 = spherely.create_point(50, 8)
+    b2 = spherely.create_point(20, 5)
     assert not spherely.intersects(a2, b2)
 
 
@@ -28,19 +28,19 @@ def test_equals() -> None:
     # test array + scalar
     a = np.array(
         [
-            spherely.linestring([(40, 8), (60, 8)]),
-            spherely.linestring([(20, 0), (30, 0)]),
+            spherely.create_linestring([(40, 8), (60, 8)]),
+            spherely.create_linestring([(20, 0), (30, 0)]),
         ]
     )
-    b = spherely.point(50, 8)
+    b = spherely.create_point(50, 8)
 
     actual = spherely.equals(a, b)
     expected = np.array([False, False])
     np.testing.assert_array_equal(actual, expected)
 
     # two scalars
-    a2 = spherely.point(50, 8)
-    b2 = spherely.point(50, 8)
+    a2 = spherely.create_point(50, 8)
+    b2 = spherely.create_point(50, 8)
     assert spherely.equals(a2, b2)
 
 
@@ -48,42 +48,42 @@ def test_contains():
     # test array + scalar
     a = np.array(
         [
-            spherely.linestring([(40, 8), (60, 8)]),
-            spherely.linestring([(20, 0), (30, 0)]),
+            spherely.create_linestring([(40, 8), (60, 8)]),
+            spherely.create_linestring([(20, 0), (30, 0)]),
         ]
     )
-    b = spherely.point(40, 8)
+    b = spherely.create_point(40, 8)
 
     actual = spherely.contains(a, b)
     expected = np.array([True, False])
     np.testing.assert_array_equal(actual, expected)
 
     # two scalars
-    a2 = spherely.linestring([(50, 8), (60, 8)])
-    b2 = spherely.point(50, 8)
+    a2 = spherely.create_linestring([(50, 8), (60, 8)])
+    b2 = spherely.create_point(50, 8)
     assert spherely.contains(a2, b2)
 
 
 def test_contains_polygon():
     # plain vs. hole polygon
-    poly_plain = spherely.polygon(shell=[(0, 0), (4, 0), (4, 4), (0, 4)])
+    poly_plain = spherely.create_polygon(shell=[(0, 0), (4, 0), (4, 4), (0, 4)])
 
-    poly_hole = spherely.polygon(
+    poly_hole = spherely.create_polygon(
         shell=[(0, 0), (4, 0), (4, 4), (0, 4)],
         holes=[[(1, 1), (3, 1), (3, 3), (1, 3)]],
     )
 
-    assert spherely.contains(poly_plain, spherely.point(2, 2))
-    assert not spherely.contains(poly_hole, spherely.point(2, 2))
+    assert spherely.contains(poly_plain, spherely.create_point(2, 2))
+    assert not spherely.contains(poly_hole, spherely.create_point(2, 2))
 
 
 def test_within():
     # test array + scalar
-    a = spherely.point(40, 8)
+    a = spherely.create_point(40, 8)
     b = np.array(
         [
-            spherely.linestring([(40, 8), (60, 8)]),
-            spherely.linestring([(20, 0), (30, 0)]),
+            spherely.create_linestring([(40, 8), (60, 8)]),
+            spherely.create_linestring([(20, 0), (30, 0)]),
         ]
     )
 
@@ -92,30 +92,30 @@ def test_within():
     np.testing.assert_array_equal(actual, expected)
 
     # two scalars
-    a2 = spherely.point(50, 8)
-    b2 = spherely.linestring([(50, 8), (60, 8)])
+    a2 = spherely.create_point(50, 8)
+    b2 = spherely.create_linestring([(50, 8), (60, 8)])
     assert spherely.within(a2, b2)
 
 
 def test_within_polygon():
     # plain vs. hole polygon
-    poly_plain = spherely.polygon(shell=[(0, 0), (4, 0), (4, 4), (0, 4)])
+    poly_plain = spherely.create_polygon(shell=[(0, 0), (4, 0), (4, 4), (0, 4)])
 
-    poly_hole = spherely.polygon(
+    poly_hole = spherely.create_polygon(
         shell=[(0, 0), (4, 0), (4, 4), (0, 4)],
         holes=[[(1, 1), (3, 1), (3, 3), (1, 3)]],
     )
 
-    assert spherely.within(spherely.point(2, 2), poly_plain)
-    assert not spherely.within(spherely.point(2, 2), poly_hole)
+    assert spherely.within(spherely.create_point(2, 2), poly_plain)
+    assert not spherely.within(spherely.create_point(2, 2), poly_hole)
 
 
 def test_disjoint():
-    a = spherely.point(40, 9)
+    a = spherely.create_point(40, 9)
     b = np.array(
         [
-            spherely.linestring([(40, 8), (60, 8)]),
-            spherely.linestring([(20, 0), (30, 0)]),
+            spherely.create_linestring([(40, 8), (60, 8)]),
+            spherely.create_linestring([(20, 0), (30, 0)]),
         ]
     )
 
@@ -124,17 +124,17 @@ def test_disjoint():
     np.testing.assert_array_equal(actual, expected)
 
     # two scalars
-    a2 = spherely.point(50, 9)
-    b2 = spherely.linestring([(50, 8), (60, 8)])
+    a2 = spherely.create_point(50, 9)
+    b2 = spherely.create_linestring([(50, 8), (60, 8)])
     assert spherely.disjoint(a2, b2)
 
 
 def test_touches():
-    a = spherely.polygon([(0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (1.0, 0.0)])
+    a = spherely.create_polygon([(0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (1.0, 0.0)])
     b = np.array(
         [
-            spherely.polygon([(1.0, 1.0), (1.0, 2.0), (2.0, 2.0), (2.0, 1.0)]),
-            spherely.polygon([(0.5, 0.5), (0.5, 1.5), (1.5, 1.5), (1.5, 0.5)]),
+            spherely.create_polygon([(1.0, 1.0), (1.0, 2.0), (2.0, 2.0), (2.0, 1.0)]),
+            spherely.create_polygon([(0.5, 0.5), (0.5, 1.5), (1.5, 1.5), (1.5, 0.5)]),
         ]
     )
 
@@ -142,20 +142,20 @@ def test_touches():
     expected = np.array([True, False])
     np.testing.assert_array_equal(actual, expected)
 
-    a_p = spherely.point(1.0, 1.0)
-    b_p = spherely.point(1.0, 1.0)
+    a_p = spherely.create_point(1.0, 1.0)
+    b_p = spherely.create_point(1.0, 1.0)
     # Points do not have a boundary, so they cannot touch per definition
     # This is consistent with PostGIS for example
     # (cmp. https://postgis.net/docs/ST_Touches.html)
     assert not spherely.touches(a_p, b_p)
 
-    b_line = spherely.linestring([(1.0, 1.0), (1.0, 2.0)])
+    b_line = spherely.create_linestring([(1.0, 1.0), (1.0, 2.0)])
     assert spherely.touches(a_p, b_line)
 
 
 @pytest.fixture
 def parent_poly():
-    return spherely.polygon(
+    return spherely.create_polygon(
         [
             (-118.0, 60.0),
             (-118.0, 40.0),
@@ -172,15 +172,15 @@ def geographies_covers_contains():
     return np.array(
         [
             # Basic point covers tests, outside, on boundary and interior
-            spherely.point(-120.0, 70.0),
-            spherely.point(-118.0, 41.0),
-            spherely.point(-116.0, 37.0),
+            spherely.create_point(-120.0, 70.0),
+            spherely.create_point(-118.0, 41.0),
+            spherely.create_point(-116.0, 37.0),
             # Basic polyline tests, crossing, on boundary and interior
-            spherely.linestring([(-120.0, 70.0), (-116.0, 37.0)]),
-            spherely.linestring([(-118.0, 41.0), (-118.0, 23.0)]),
-            spherely.linestring([(-117.0, 39.0), (-115.0, 37.0)]),
+            spherely.create_linestring([(-120.0, 70.0), (-116.0, 37.0)]),
+            spherely.create_linestring([(-118.0, 41.0), (-118.0, 23.0)]),
+            spherely.create_linestring([(-117.0, 39.0), (-115.0, 37.0)]),
             # Basic polygon test, crossing, shared boundary and interior
-            spherely.polygon(
+            spherely.create_polygon(
                 [(-120.0, 41.0), (-120.0, 35.0), (-115.0, 35.0), (-115.0, 41.0)]
             ),
             # TODO: This case is currently not fully correct. Supplying a
@@ -197,10 +197,10 @@ def geographies_covers_contains():
             # be covered anymore, even if it would be in reality. Therefor,
             # these tests assume identical shared edges between polygons `a` and `b`,
             # which does work as intended.
-            spherely.polygon(
+            spherely.create_polygon(
                 [(-118.0, 40.0), (-118.0, 23.0), (34.0, 23.0), (34.0, 40.0)]
             ),
-            spherely.polygon(
+            spherely.create_polygon(
                 [(-117.0, 40.0), (-117.0, 35.0), (-115.0, 35.0), (-115.0, 40.0)]
             ),
         ]
